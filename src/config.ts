2a951d6443933f70ca4b4d549b73081eb60318b9
Harrison Borges <hborges005@gmail.com>
2022-11-07 17:26:35 +0100

    add config file

---
+++ src/config.ts
@@ -0,0 +1,15 @@
+const config = {
+	nodeEnv: process.env.NODE_ENV ?? 'development',
+	port: process.env.PORT ?? 8000,
+	databaseUrl: process.env.DATABASE_URL ?? '',
+	databaseConnectionLimit: parseInt(
+		process.env.DATABASE_CONNECTION_LIMIT ?? '20'
+	),
+
+	clientOrigins: {
+		development: process.env.DEV_ORIGIN ?? '*',
+		production: process.env.PROD_ORIGIN ?? 'none',
+	},
+}
+
+export default config
