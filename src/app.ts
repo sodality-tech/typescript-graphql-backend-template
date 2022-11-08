bcc9c25ef48d88bdb647fe32cd966a88ae1bf4e2
Harrison Borges <hborges005@gmail.com>
2022-11-08 13:10:46 +0100

    add default configuration for express app

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,10 @@ import express from 'express'
 import graphqlServer from './graphql'
 
 const app = express()
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: false }))
+app.disable('x-powered-by')
 
 graphqlServer.start().then(() => {
 	graphqlServer.applyMiddleware({
