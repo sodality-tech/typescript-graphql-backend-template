459c4f4ebab26788635a9c6c9b5f2447ab6bec3a
Harrison Borges <hborges005@gmail.com>
2022-11-13 22:42:08 +0000

    Update user.test.ts

--- src/graphql/resolvers/user/user.test.ts
+++ src/graphql/resolvers/user/user.test.ts
@@ -6,31 +6,30 @@ const testServer = new ApolloServer({
 	schema,
 })
 
-beforeAll((done) => {
-	testServer.start().then(() => {
-		done()
-	})
+beforeAll(async () => {
+	await testServer.start()
 })
 
-afterAll((done) => {
-	testServer.stop().then(() => {
-		done()
-	})
+afterAll(async () => {
+	await testServer.stop()
 })
 
-test('returns hello with the provided name', async () => {
-	const response = await testServer.executeOperation({
-		query: 'query CurrentUser { currentUser { id } }',
-	})
+describe('me endpoint', () => {
+	const query = 'query Me { me { id } }'
 
-	// Note the use of Node's assert rather than Jest's expect; if using
-	// TypeScript, `assert`` will appropriately narrow the type of `body`
-	// and `expect` will not.
-	assert(response.body.kind === 'single')
+	test('returns hello with the provided name', async () => {
+		const response = await testServer.executeOperation({
+			query,
+		})
 
-	expect(response.body).not.toBeNull()
-	expect(response.body.singleResult.errors).toBeUndefined()
-	expect(response.body.singleResult.data?.currentUser).toEqual({
-		id: 'user-id',
+		// Note the use of Node's assert rather than Jest's expect; if using
+		// TypeScript, `assert` will appropriately narrow the type of `body`
+		// and `expect` will not.
+		assert(response.body.kind === 'single')
+
+		expect(response.body.singleResult.errors).toBeUndefined()
+		expect(response.body.singleResult.data?.me).toEqual({
+			id: 'user-id',
+		})
 	})
 })
